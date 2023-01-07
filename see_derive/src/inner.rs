da1e5052ed9fe75c5c1b74e6859cda065646655d
Nishant Joshi <nishantjo.12@gmail.com>
2023-01-07 11:04:43 +0530

    fix: repair fmt of the crate

--- see_derive/src/inner.rs
+++ see_derive/src/inner.rs
@@ -1,25 +1,24 @@
-use proc_macro2::{TokenStream, Ident, Span};
+use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
-use syn::{DeriveInput, spanned::Spanned};
 use std::sync;
+use syn::{spanned::Spanned, DeriveInput};
 
 static FIELDS: sync::Mutex<Vec<String>> = sync::Mutex::new(Vec::new());
-
-
 
 pub(crate) fn see_derive(input: DeriveInput) -> Result<TokenStream, syn::Error> {
     let name = &input.ident;
     let fields = match input.data {
-        syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(syn::FieldsNamed { named, .. }), .. }) => {
-            named.into_iter()
-                .filter_map(|field| {
-                    field.ident.map(|idn| (idn, field.ty))
-                })
-                .map(|(ident, ty)| {
-                    (field_consumer(ident), ty)
-                })
-        }
-        _ => Err(syn::Error::new(input.clone().span(), "The datatype should be a struct with named fields"))?
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+            ..
+        }) => named
+            .into_iter()
+            .filter_map(|field| field.ident.map(|idn| (idn, field.ty)))
+            .map(|(ident, ty)| (field_consumer(ident), ty)),
+        _ => Err(syn::Error::new(
+            input.clone().span(),
+            "The datatype should be a struct with named fields",
+        ))?,
     };
     let (field, types) = split_iter(fields);
     let (old_f, new_f) = split_iter(field);
@@ -62,7 +61,7 @@ fn field_consumer(idn: Ident) -> (Ident, Ident) {
     match store.contains(&idn.to_string()) {
         true => (),
         false => {
-            store.push(idn.to_string()); 
+            store.push(idn.to_string());
         }
     }
     (idn.clone(), field_to_ident(&idn.to_string(), idn.span()))
@@ -74,22 +73,20 @@ fn field_to_ident<'a>(name: &'a str, span: Span) -> Ident {
 
 fn create_struct_stream(span: Span) -> TokenStream {
     let structs = FIELDS.lock().unwrap();
-    let structs = structs
-        .iter().map(|name| {
-            field_to_ident(name, span)
-        });
+    let structs = structs.iter().map(|name| field_to_ident(name, span));
     quote! {
         #(pub struct #structs;)*
     }
 }
 
-fn split_iter<T, U>(input_iter: impl Iterator<Item = (T, U)>) -> (impl Iterator<Item = T>, impl Iterator<Item = U>) {
+fn split_iter<T, U>(
+    input_iter: impl Iterator<Item = (T, U)>,
+) -> (impl Iterator<Item = T>, impl Iterator<Item = U>) {
     let (mut s1, mut s2) = (Vec::new(), Vec::new());
     input_iter.for_each(|(t, u)| {
         s1.push(t);
         s2.push(u);
     });
-
 
     (s1.into_iter(), s2.into_iter())
 }
--- see_derive/src/lib.rs
+++ see_derive/src/lib.rs
@@ -1,16 +1,15 @@
 //!
 //! Derive For the traits `See` and an extension trait `Load`
-//! 
+//!
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 mod inner;
 
-
 ///
 /// Extension of the trait `See`, providing auto implementation for `See`,
 /// while also combining the loading visitors into a single place.
-/// 
+///
 /// ## Example
 /// ```rust
 /// #[derive(see_derive::See)]
@@ -20,36 +19,36 @@ mod inner;
 /// }
 /// ```
 /// This above example implements `See<crate::see_t::__x>` and `See<crate::see_t::__y>` for the point struct
-/// 
+///
 #[proc_macro_derive(See)]
 pub fn see_derive(input: TokenStream) -> TokenStream {
     match inner::see_derive(parse_macro_input!(input as DeriveInput)) {
         Ok(res) => res,
-        Err(err) => err.into_compile_error()
-    }.into()
+        Err(err) => err.into_compile_error(),
+    }
+    .into()
 }
-
 
 ///
 /// The derive macro includes the visitors in your codebase. Which
 /// are created during the use of `#[derive(See)]`
-/// 
+///
 /// ## Example
 /// This derive must be used at the end of the origin location of your current crate, i.e. `lib.rs` or `main.rs`
 /// or a way that will allow use to access the data as `crate::see_t`
 /// ```rust
 /// // In lib.rs
-/// 
+///
 /// // .. your code goes here
-/// 
-/// 
+///
+///
 /// pub(crate) mod see_t {
 ///     #[derive(see_derive::Load)]
 ///     struct SeeT;
 /// }
 /// ``
-/// 
-/// 
+///
+///
 #[proc_macro_derive(Load)]
 pub fn load_fields(input: TokenStream) -> TokenStream {
     inner::load_fields(parse_macro_input!(input as DeriveInput)).into()
